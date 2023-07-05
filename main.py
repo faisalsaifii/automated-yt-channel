a2efe58eb3acb337d7a8cb90726d424194719e30
faisalsaifii <m.faisal.s@outlook.com>
2023-07-05 16:13:03 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import schedule
+import time
+import datetime
+import os
+import shutil
+import config
+
+months = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    12: 'Nov',
+    11: 'Dec',
+}
+
+IG_USERNAME = config.IG_USERNAME
+IG_PASSWORD = config.IG_PASSWORD
